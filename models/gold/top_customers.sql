ee4ef646fd1e8fabfef1ec23dc7813ccd5f7cbce
Apostolos Athanasiou <apostolos1927@gmail.com>
2025-11-19 12:48:01 +0000

    test

--- models/gold/top_customers.sql
+++ models/gold/top_customers.sql
@@ -4,6 +4,6 @@ select
   concat(c.first_name,' ',c.last_name) as customer_name,
   sum(o.total_amount) as lifetime_spend
 from {{ ref('fct_orders') }} o
-join {{ ref('dim_customers') }} c on o.customer_id = c.customer_id
+join {{ ref('customers_snapshot') }} c on o.customer_id = c.customer_id and c.dbt_valid_to is null
 group by c.customer_sk, c.customer_id, c.first_name, c.last_name
 order by lifetime_spend desc;
